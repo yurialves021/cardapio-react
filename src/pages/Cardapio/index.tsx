3346f4ed909acbf356de671e58a37f90f0c114a7
Yuri Alves <yurialvesedf@gmail.com>
2022-11-03 16:46:54 -0300

    Refatorando o Menu e o Header com rotas

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,5 +1,4 @@
 import styles from './Cardapio.module.scss';
-import { ReactComponent as Logo } from 'assets/img/logo2.svg';
 import Buscador from 'pages/Buscador';
 import Filtros from 'pages/Filtros';
 import { useState } from 'react';
@@ -14,38 +13,31 @@ export default function Cardapio() {
   const [ordenador, setOrdenador] = useState('');
 
   return (
-    <main>
-      <nav className={styles.menu}><Logo /></nav>
 
-      <header className={styles.header}>
-        <div className={styles.header__text}>
-                    Cardápio de Massas
-        </div>
-      </header>
-      <section className={styles.cardapio}>
-        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-        <Buscador
-          busca={busca}
-          setBusca={setBusca}
-        />
-        <div className={styles.cardapio__filtros}>
-          <Filtros
-            filtro={filtro}
-            setFiltro={setFiltro}
-          />
-          <Ordenador
-            ordenador={ordenador}
-            setOrdenador={setOrdenador}
-          />
-        </div>
-        <Itens
-          busca={busca}
+    <section className={styles.cardapio}>
+      <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+      <Buscador
+        busca={busca}
+        setBusca={setBusca}
+      />
+      <div className={styles.cardapio__filtros}>
+        <Filtros
           filtro={filtro}
-          ordenador={ordenador}
+          setFiltro={setFiltro}
         />
+        <Ordenador
+          ordenador={ordenador}
+          setOrdenador={setOrdenador}
+        />
+      </div>
+      <Itens
+        busca={busca}
+        filtro={filtro}
+        ordenador={ordenador}
+      />
 
-      </section>
+    </section>
 
-    </main>
+
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,23 @@
+import Menu from 'components/Menu';
+import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio/indext';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default function AppRouter() {
   return (
+    <main>
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path='cardapio' element={<Cardapio />} />
+          </Route>
 
-    <Router>
-      <Routes>
-        <Route path='/' element={<Inicio />}/>
-        <Route path='/cardapio' element={<Cardapio />} />
-      </Routes>
-    </Router>
-
+        </Routes>
+      </Router>
+    </main>
 
   );
 }
