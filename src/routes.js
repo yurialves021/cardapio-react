723197324a4f4e796571aac879da93618212c8b2
Yuri Alves <yurialvesedf@gmail.com>
2022-11-07 12:05:46 -0300

    Criando o botão voltar

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,13 @@ import Menu from 'components/Menu';
 import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio/indext';
+import NotFound from 'pages/NotFound';
 import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default function AppRouter() {
   return (
-    <main>
+    <main className='container'>
       <Router>
         <Menu />
         <Routes>
@@ -17,7 +18,7 @@ export default function AppRouter() {
             <Route path='cardapio' element={<Cardapio />} />
             <Route path='sobre' element={<Sobre />} />
           </Route>
-       
+          <Route path='*' element={<NotFound />} />
         </Routes>
         <Footer />
       </Router>
